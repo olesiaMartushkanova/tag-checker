4dc37c777951faac817c78988449b0836f457d65
olesiaMartushkanova <olesia.martushkanova@seeeverything.com>
2022-06-19 20:54:11 +1200

    tests

--- src/tagChecker.test.ts
+++ src/tagChecker.test.ts
@@ -1,8 +1,33 @@
 import { tagChecker } from './tagChecker';
 
-describe('tagChecker', () => {
-  it('wrong mixed tag', () => {
-    const actual = tagChecker('<A></A><B></B>');
-    expect(actual).toBe(false);
+describe.each([
+  ['<A></A><B></B>', 'Correctly tagged paragraph'],
+  ['<A><B><C></C></B></A>', 'Correctly tagged paragraph'],
+  [
+    'The following text<C><B>is centred and in boldface</B></C>',
+    'Correctly tagged paragraph',
+  ],
+  [
+    '<B>This <g>is <B>boldface</B> in <<*> a</B> <\6> <<d>sentence',
+    'Correctly tagged paragraph',
+  ],
+  [
+    '<B><C> This should be centred and in boldface, but the tags are wrongly nested </B></C>',
+    'Expected </C> found </B>',
+  ],
+  [
+    '<B>This should be in boldface, but there is an extra closing tag</B></C>',
+    'Expected # found </C>',
+  ],
+  [
+    '<B><C>This should be centred and in boldface, but there is a missing closing tag</C>',
+    'Expected </B> found #',
+  ],
+])('tagChecker', (input, expectedLogMessage) => {
+  it('returns correct message', () => {
+    console.log = jest.fn();
+
+    tagChecker(input);
+    expect(console.log).toHaveBeenCalledWith(expectedLogMessage);
   });
 });
